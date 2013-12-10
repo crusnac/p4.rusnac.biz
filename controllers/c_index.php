383cd85a8d35ec09e536bddca274e0894c528959
Claudiu Rusnac <claudiu@rusnac.net>
2013-12-10 09:51:15 -0800

    Initial Commit

--- controllers/c_index.php
+++ controllers/c_index.php
@@ -12,25 +12,56 @@ class index_controller extends base_controller {
 	/*-------------------------------------------------------------------------------------------------
 	Accessed via http://localhost/index/index/
 	-------------------------------------------------------------------------------------------------*/
-	public function index() {
+	public function index(){
 		
-		# Any method that loads a view will commonly start with this
-		# First, set the content of the template with a view file
-			$this->template->content = View::instance('v_index_index');
-			
-		# Now set the <title> tag
-			$this->template->title = "Hello World";
+		//Redirect to the Dashoard page
+		Router::redirect('/index/dashboard/');
+		
+		
+	}//End of Function
 	
-		# CSS/JS includes
-			/*
-			$client_files_head = Array("");
-	    	$this->template->client_files_head = Utils::load_client_files($client_files);
-	    	
-	    	$client_files_body = Array("");
-	    	$this->template->client_files_body = Utils::load_client_files($client_files_body);   
-	    	*/
-	      					     		
-		# Render the view
+	
+	
+	public function dashboard($page = NULL) {
+		
+			//Set Main Template.
+			$this->template->content = View::instance('v_index');
+			
+			# Now set the <title> tag
+			$this->template->title = "Hello World";
+				
+		
+			//Query the Reports table to and return the Array.  Sort of the file timestamp.
+			$q = "select * from reports ORDER BY file_timestamp DESC";	
+			$reports = DB::instance(DB_NAME)->select_rows($q);	
+						
+			// #### Set Pagination Variables #### //
+			
+				//Items per page
+				$items_per_page = 15;
+				
+				//Total number of reports
+				$total_reports = count($reports);
+				
+				//Set total number of pages derived by dividing total reports by the items_per_page variable.
+				$total_pages = ceil($total_reports / $items_per_page);
+				
+				//Specify the Current Page - passed via URL parameter ex. ?page=1
+				$current_page = $page;
+				
+				//End Number of the array to display
+				$offset = ($current_page * $items_per_page) + $items_per_page;
+				
+				//Beginning number of the array
+				$start_offset = $offset - $items_per_page;
+				
+				//Sliced array using the start and end offset.  False will not preserve the array keys. http://php.net/array_slice
+				$reports_paged = array_slice($reports, $start_offset, $items_per_page, false);
+			
+			//Send Sliced array to the View
+			$this->template->content->reports = $reports_paged;
+			      					     		
+			// Render the view
 			echo $this->template;
 
 	} # End of method
