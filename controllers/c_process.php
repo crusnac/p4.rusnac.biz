822e52e269975aedf06104f5161315d38214965b
Claudiu Rusnac <claudiu@rusnac.net>
2013-12-10 09:51:36 -0800

    Cleaned up

--- controllers/c_process.php
+++ controllers/c_process.php
@@ -9,24 +9,21 @@ class process_controller extends base_controller {
 		//Set path of unprocessed XML files.
 		$xmldir = APP_PATH.'xml/';
 		
-		//Set arrays for each of the vulnerability criticalities.  This will be needed for the counts.
-		$criticalVulns = array();
-		$highVulns = array();
-		$mediumVulns = array();
-		$lowVulns = array();
-		$informationalVulns = array();
 		
+		//Check to see if there are any .xml files in the XML directory, if not redirect 
 		if (count(glob($xmldir."*.xml")) === 0 ) { // empty
 		
+			//Redirect to report list with error
 			echo "Directory is Empty";
 		}
 		
-				
+			//If Directory exsits.
 			if (is_dir($xmldir)) {
 			
 				//Open Directory Stream
 			    if ($xml = opendir($xmldir)) {
-			        	
+			        
+			        //Go through each file and process....
 			        while (($file = readdir($xml)) !== false) {
 			        		
 			        	//Only show files with a .xml extension	    	
@@ -34,35 +31,43 @@ class process_controller extends base_controller {
 			            	
 			            	$filexml = simplexml_load_file($xmldir.$file);
 			            	
-			            	//Process Critical Vulns and put each record into an array				            				            	
+			            	
+			            	//Set arrays for each of the vulnerability criticalities.  This will be needed for the counts.
+							$criticalVulns = array();
+							$highVulns = array();
+							$mediumVulns = array();
+							$lowVulns = array();
+							$informationalVulns = array();
+			            				            	
+			            	//Process Critical Vulns and put each record into an array				            				            
 			            	foreach ($filexml->Issues->Issue as $severity) {
 			            			if ($severity->Severity == 4) {
 			            				$criticalVulns[] = $severity->Severity;
 			            			}
 			      			}
 			      			
-			      			//Process High Vulns and put each record into an array				            				            	
+			      			//Process High Vulns and put each record into an array				            				            
 			            	foreach ($filexml->Issues->Issue as $severity) {
 			            			if ($severity->Severity == 3) {
 			            				$highVulns[] = $severity->Severity;
 			            			}
 			      			}
 			      			
-			      			//Process Medium Vulns and put each record into an array				            				            	
+			      			//Process Medium Vulns and put each record into an array				            				            
 			            	foreach ($filexml->Issues->Issue as $severity) {
 			            			if ($severity->Severity == 2) {
 			            				$mediumVulns[] = $severity->Severity;
 			            			}
 			      			}
 			      			
-			      			//Process Low Vulns and put each record into an array				            				            	
+			      			//Process Low Vulns and put each record into an array				            				            
 			            	foreach ($filexml->Issues->Issue as $severity) {
 			            			if ($severity->Severity == 1) {
 			            				$lowVulns[] = $severity->Severity;
 			            			}
 			      			}
 			      			
-			      			//Process Informational Vulns and put each record into an array				            				            	
+			      			//Process Informational Vulns and put each record into an array				            				            
 			            	foreach ($filexml->Issues->Issue as $severity) {
 			            			if ($severity->Severity == 0) {
 			            				$informationalVulns[] = $severity->Severity;
@@ -70,48 +75,64 @@ class process_controller extends base_controller {
 			      			}
 							
 							
-							// ##### Create variables that will be written to the DB.
+							// ##### Create variables that will be written to the DB. ##### //
 							
-							//Scan Request number
-							$request_number = intval($filexml->Name);
-							
-							//Scanned URL - Filtered from the file to only display the URL portion of the string.
-							preg_match_all('~(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?)~', $filexml->Name, $scanned_url_filtered);
-							$scanned_url = reset($scanned_url_filtered);
-							$scanned_url = $scanned_url[0];
-							
-							//Date Created
-							$created = Time::now();
-							
-							//Scan Start Time
-							$start_time = $filexml->StartTime;
-							
-							//Scan Duration
-							$duration = $filexml->Duration;
-							
-							//Clean up filename and convert filename to a Hash - example - 1b899bfb07d8158ced56fa2738fbc45675275fbb89528621f8a6c2da206a4694-43473.xml
-							$file = str_replace(' ','-', $file);
-							
-							//Create the hash of the file with -request_number.xml
-							$hash = hash('sha256', $xmldir.$file);	
-							$filename = $hash."-$file";
-				           				                                 
-							
-							//Vulnerability Counts
-							$vulns_critical = count($criticalVulns);
-							$vulns_high = count($highVulns);			            	
-							$vulns_medium = count($mediumVulns);			            	
-							$vulns_low =count($lowVulns);			            	
-			            	$vulns_informational = count($informationalVulns);
+								//Scan Request number
+								$request_number = intval($filexml->Name);
+								
+								//Scanned URL - Filtered from the file to only display the URL portion of the string.
+								preg_match_all('~(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?)~', $filexml->Name, $scanned_url_filtered);
+								$scanned_url = reset($scanned_url_filtered);
+								$scanned_url = $scanned_url[0];
+								
+								//Date Created
+								$created = Time::now();
+								
+								//Policy
+								$policy = $filexml->PolicyName;
+								
+								//Scan Name
+								$scan_name = $filexml->Name;
+								
+								//Scan Start Time
+								$start_time = $filexml->StartTime;
+								
+								//Scan Duration
+								$duration = $filexml->Duration;
+								
+								//File Modified Timestamp								
+								$file_timestamp = filemtime($xmldir.$file);
+								
+								//Filesize in bytes								
+								$filesize = filesize($xmldir.$file);
+								
+								//Clean up filename and convert filename to a Hash - example - 1b89.....6c2da206a4694-43473.xml
+								$file = str_replace(' ','-', $file);
+								
+								//Create the hash of the file with -request_number.xml
+								$hash = hash_file('sha256', $xmldir.$file);	
+								$filename = $hash."-$file";
+
+								
+								//Vulnerability Counts
+								$vulns_critical = count($criticalVulns);
+								$vulns_high = count($highVulns);			            	
+								$vulns_medium = count($mediumVulns);			            	
+								$vulns_low =count($lowVulns);			            	
+				            	$vulns_informational = count($informationalVulns);
 							
 														
 							// Insert data into the database for each report.
 							$data = Array(	'request_number' => $request_number, 
 											'scanned_url' => $scanned_url, 
+											'scan_name' => $scan_name, 
 											'created' => $created, 
+											'policy' => $policy, 
 											'start_time' => $start_time,
 											'duration' => $duration,
+											'file_timestamp' => $file_timestamp,
 											'filename' => $filename,
+											'filesize' => $filesize,
 											'vulns_critical' => $vulns_critical,
 											'vulns_high' => $vulns_high,
 											'vulns_medium' => $vulns_medium,
@@ -131,10 +152,32 @@ class process_controller extends base_controller {
 			        
 			        //Close Directory Stream
 			        closedir($xml);
-			    }
-			}
+		    }
+		}
+			
+			//Redirect to the report list
+			// This needs to be added or maybe not if AJAX is implemented.
+			
 
 	}// End of Fuction
+	
+	
+	########### //Delete Cache Files ###########
+	public function cache() {
+		
+		echo "Cache Function";
+	
+	}
+	
+	
+	########### //Archive Files ###########
+	public function archive() {
+		echo "Archive Function";
+	
+	}
+	
+	
+	
 
 	
 	
