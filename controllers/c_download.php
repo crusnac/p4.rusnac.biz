636b9fb8d01fbda29b604c6bc609ed061d381964
Claudiu Rusnac <claudiu@rusnac.net>
2013-12-10 09:52:26 -0800

    Initial commit - created download functions for CSV and HTML files.

---
+++ controllers/c_download.php
@@ -0,0 +1,72 @@
+<?php
+
+class download_controller extends base_controller {
+	
+
+	########### //Process the XML and Send to View ###########
+	public function csv($report = NULL) {
+
+		//Define location of the process file directory.
+		$file = 'csv/'.$report.'.csv';
+		
+		//Check to See if the File Exsits... if so, send the file to the user.			
+		if (file_exists($file))
+			{
+			    header('Content-Description: File Transfer');
+			    header('Content-Type: application/octet-stream');
+			    header('Content-Disposition: attachment; filename='.basename($file));
+			    header('Content-Transfer-Encoding: binary');
+			    header('Expires: 0');
+			    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+			    header('Pragma: public');
+			    header('Content-Length: ' . filesize($file));
+			    ob_clean();
+			    flush();
+			    readfile($file);
+			    exit;
+			
+			}else{
+			
+			    //Redirect because the files does not exsit.
+			    echo "File does not exists";
+			}		
+
+	}// End of Fuction
+	
+	########### //Process the XML and Send to View ###########
+	public function html($report = NULL) {
+	
+		//Define location of the process file directory.
+		$file = 'html/'.$report.'.html';
+		
+		//Check to See if the File Exsits... if so, send the file to the user.			
+		if (file_exists($file))
+			{
+			    header('Content-Description: File Transfer');
+			    header('Content-Type: application/octet-stream');
+			    header('Content-Disposition: attachment; filename='.basename($file));
+			    header('Content-Transfer-Encoding: binary');
+			    header('Expires: 0');
+			    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+			    header('Pragma: public');
+			    header('Content-Length: ' . filesize($file));
+			    ob_clean();
+			    flush();
+			    readfile($file);
+			    exit;
+			
+			}else{
+			
+			    //Redirect because the files does not exsit.
+			    echo "File does not exists";
+			}		
+		
+	
+		
+	
+	}//End of function
+	
+		
+	
+	
+} # End of class
