a4e7d5af097df12c606013421bb4d1aa73092522
Claudiu Rusnac <claudiu@rusnac.net>
2013-12-13 11:32:07 -0800

    Added links to sidemenu to support new functionality

--- views/v_index_reports_sidemenu.php
+++ views/v_index_reports_sidemenu.php
@@ -3,7 +3,11 @@
 	<li class="<?php if (strpos($_SERVER['REQUEST_URI'], '/index/reports/') !== FALSE){print "active";} ?>"><a href="/index/reports/"><i class="fa fa-table"></i> Reports</a></li>
 	<li class="<?php if (strpos($_SERVER['REQUEST_URI'], '/index/statistics/') !== FALSE){print "active";} ?>"><a href="/index/statistics/"><i class="fa fa-bar-chart-o"></i> Statistics</a></li>
 	<li class="<?php if (strpos($_SERVER['REQUEST_URI'], '/index/search/') !== FALSE){print "active";} ?>"><a href="/index/search/"><i class="fa fa-search"></i> Search</a></li>
+	
+	<li class="<?php if (strpos($_SERVER['REQUEST_URI'], '/index/upload/') !== FALSE){print "active";} ?>"><a href="/index/upload/"><i class="fa fa-cloud-upload"></i> Upload</a></li>
     
+    	<li class="<?php if (strpos($_SERVER['REQUEST_URI'], '/index/users/') !== FALSE){print "active";} ?>"><a href="/index/users/"><i class="fa fa-users"></i> Users</a></li>
+
     
     
     
