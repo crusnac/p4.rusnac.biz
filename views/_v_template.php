36822d8ff0b53670260ba3c4127ec85d4b7b12eb
Claudiu Rusnac <claudiu@rusnac.net>
2013-12-19 08:32:53 -0800

    Cleaned up

--- views/_v_template.php
+++ views/_v_template.php
@@ -1,22 +1,56 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title><?php if(isset($title)) echo $title; ?></title>
-	
-	    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?=APP_NAME?> | <?php if(isset($title)) echo $title; ?></title>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href="http://bootswatch.com/yeti/bootstrap.min.css">
+
+    <!-- Add custom CSS here -->
+    <link href="/css/dashboard.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    
+        <!-- JavaScript -->
+	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+	<script src="/js/dashboard.js"></script>
+
+  </head>
+
+  <body>
 
 
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
-					
-	<!-- Controller Specific JS/CSS -->
-	<?php if(isset($client_files_head)) echo $client_files_head; ?>
-	
-</head>
+      <!-- Sidebar -->
+      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <a class="navbar-brand" href="/"><i class="fa fa-cogs"></i> <?=APP_NAME?></title>
+</a>
+        </div>
+        
+      </nav>
 
-<body>	
+      <div id="page-wrapper">
 
-	<?php if(isset($content)) echo $content; ?>
+        <div class="row">
+          <div class="col-lg-12">      
+                
+            
+            <?php if(isset($content)) echo $content; ?>
+			<?php if(isset($client_files_body)) echo $client_files_body; ?>            
+            
+            
+          </div>
+        </div><!-- /.row -->
 
-	<?php if(isset($client_files_body)) echo $client_files_body; ?>
-</body>
+      </div><!-- /#page-wrapper -->
+
+    </div><!-- /#wrapper -->
+
+  </body>
 </html>
