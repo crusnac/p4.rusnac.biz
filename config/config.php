8fdfab8fb6b9d48000804c09cb241cd34e46ba43
Claudiu Rusnac <claudiu@rusnac.net>
2013-12-19 08:27:40 -0800

    Changed Application Name

--- config/config.php
+++ config/config.php
@@ -14,7 +14,7 @@ If you want a different default time format for this app, set it below
 */
 
 # What is the name of this app?
-	define('APP_NAME', 'Web Application Report Engine ');  
+	define('APP_NAME', 'Web Application Assessment Report Management Platform');  
 
 # When email is sent out from the server, where should it come from?
 # Ideally, this should match the domain name
