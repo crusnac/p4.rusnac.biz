d1326fec3f0579be9fae05bae544dabc858df82a
Claudiu Rusnac <claudiu@rusnac.net>
2013-12-19 08:32:38 -0800

    Cleaned up

--- views/_v_report_template.php
+++ views/_v_report_template.php
@@ -70,34 +70,6 @@
 				border-left: 1px solid #dddddd; border-right: 1px solid #dddddd; border-bottom: 1px solid #dddddd; margin-bottom: 10px;
 				}
 	
-	</style>
-	
-	
-	<!-- Font Awesome CSS -->
-	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    
-	<!-- Google Charts -->
-	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
-
-
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
-					
-	<!-- Controller Specific JS/CSS -->
-	<?php if(isset($client_files_head)) echo $client_files_head; ?>
-	
-	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
-    
-    	<style>
-	
 	/* Sticky footer styles -------------------------------------------------- */
 		html,
 		body {
@@ -141,11 +113,32 @@
 		code {
 		  font-size: 80%;
 		}
-
 	</style>
 	
-
 	
+	<!-- Font Awesome CSS -->
+	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    
+	<!-- Google Charts -->
+	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
+					
+	<!-- Controller Specific JS/CSS -->
+	<?php if(isset($client_files_head)) echo $client_files_head; ?>
+	
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->	
+
 </head>
 
 <body>	
