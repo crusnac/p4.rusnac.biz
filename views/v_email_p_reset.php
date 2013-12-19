5c76d3bb75b725fefb591b76705649f4a2409df0
Claudiu Rusnac <claudiu@rusnac.net>
2013-12-19 08:51:02 -0800

    Changed link.

--- views/v_email_p_reset.php
+++ views/v_email_p_reset.php
@@ -3,4 +3,4 @@
 <p>
 You can reset you password by following the link below.
 </p>
-<a href="<?php print APP_URL; ?><?php echo $token; ?>/"><?php print APP_URL; ?><?php echo $token; ?>/</a>
+<a href="<?php print APP_URL; ?>password/set/<?php print $token; ?>/">Reset My Password</a>
