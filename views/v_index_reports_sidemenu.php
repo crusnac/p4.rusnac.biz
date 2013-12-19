587b9e9f1aeafa2d5f6243c2fe5a17d1f2290052
Claudiu Rusnac <claudiu@rusnac.net>
2013-12-19 08:33:40 -0800

    Added menu items.

--- views/v_index_reports_sidemenu.php
+++ views/v_index_reports_sidemenu.php
@@ -5,12 +5,22 @@
 	<li class="<?php if (strpos($_SERVER['REQUEST_URI'], '/index/search/') !== FALSE){print "active";} ?>"><a href="/index/search/"><i class="fa fa-search"></i> Search</a></li>
 	
 	<li class="<?php if (strpos($_SERVER['REQUEST_URI'], '/index/upload/') !== FALSE){print "active";} ?>"><a href="/index/upload/"><i class="fa fa-cloud-upload"></i> Upload</a></li>
-    
-    	<li class="<?php if (strpos($_SERVER['REQUEST_URI'], '/index/users/') !== FALSE){print "active";} ?>"><a href="/index/users/"><i class="fa fa-users"></i> Users</a></li>
-
+	<li class="<?php if (strpos($_SERVER['REQUEST_URI'], '/index/users/') !== FALSE){print "active";} ?>"><a href="/index/users/"><i class="fa fa-users"></i> Users</a></li>
     
     
     
-        
     
+   <!-- User Related -->         
+    	                
+		<li class="dropdown">
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $user->first_name; ?> <?php echo $user->last_name; ?> <b class="caret"></b></a>
+				<ul class="dropdown-menu">
+	                <li><a href="/users/profile/"><i class="fa fa-user"></i> Profile</a></li>
+	                <li><a href="/users/logout/"><i class="fa fa-power-off"></i> Log Out</a></li>
+	                <li class="divider"></li>
+              </ul>
+            </li>
 </ul>
+
+
+
