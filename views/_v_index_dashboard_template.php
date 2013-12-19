fb121c71b362d3e875b96d8e6b3d55b05f422406
Claudiu Rusnac <claudiu@rusnac.net>
2013-12-19 08:32:23 -0800

    cleaned-up

--- views/_v_index_dashboard_template.php
+++ views/_v_index_dashboard_template.php
@@ -5,10 +5,10 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Web Application Assessment Report Engine</title>
+    <title><?=APP_NAME?> | <?php if(isset($title)) echo $title; ?></title>
 
 	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
 	<link rel="stylesheet" href="http://bootswatch.com/yeti/bootstrap.min.css">
 
     <!-- Add custom CSS here -->
@@ -17,7 +17,7 @@
     
         <!-- JavaScript -->
 	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 	<script src="/js/dashboard.js"></script>
 
   </head>
@@ -36,7 +36,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#"><i class="fa fa-cogs"></i> Web Application Assessment Report Management Platform</a>
+          <a class="navbar-brand" href="/"><i class="fa fa-cogs"></i> <?=APP_NAME?></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
@@ -53,7 +53,15 @@
       <div id="page-wrapper">
 
         <div class="row">
-          <div class="col-lg-12">            
+          <div class="col-lg-12"> 
+          
+          <?php if ($unprocessed_reports == '0'): ?>
+	
+		<?php else: ?>
+		
+			<div class="alert alert-info"><i class="fa fa-exclamation-triangle"></i> <strong>There appears to be some unprocessed reports. <a href="/process/reports/">Process Reports</a></strong></div>
+		
+	<?php endif; ?>    
             
             <?php if(isset($content)) echo $content; ?>
 			<?php if(isset($client_files_body)) echo $client_files_body; ?>
