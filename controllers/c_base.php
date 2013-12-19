2f5d30fa157a23e8eabe4b0ff216914f0836d016
Claudiu Rusnac <claudiu@rusnac.net>
2013-12-19 08:28:54 -0800

    Added Unprocessed Variable
    To display a notice is there are unprocessed reports in the xml
    directory.  The functionality is used if files are placed in the xml
    directory.  Upload functionallity will automatically process the script.

--- controllers/c_base.php
+++ controllers/c_base.php
@@ -6,7 +6,8 @@ class base_controller {
 	public $userObj;
 	public $template;
 	public $email_template;
-
+	public $unprocessed_reports;
+	
 	/*-------------------------------------------------------------------------------------------------
 
 	-------------------------------------------------------------------------------------------------*/
@@ -25,6 +26,19 @@ class base_controller {
 		# So we can use $user in views			
 			$this->template->set_global('user', $this->user);
 			
+		# Determine if there is unprocessed XML files in the XML directory and specify the count to be processed in the view.
+			
+			//Set Variables.
+			$reportdir = APP_PATH.'xml/';
+			
+			//Look for only .xml files.
+			$files = glob($reportdir.'*.xml');
+			//Count!
+			$unprocessed_reports = count($files);
+			
+			//Set the variable to the template to be used for processing.
+			$this->template->set_global('unprocessed_reports', $unprocessed_reports);
+						
 	}
 	
 } # eoc
